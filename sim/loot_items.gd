13bb4b55de15b93006c26fd44cedc0514e45eba0
dmitriikomarskikh-coder <dmitrii.komarskikh@gmail.com>
2026-06-13 11:14:19 +0300

    Add sim/loot_items.gd

---
+++ sim/loot_items.gd
@@ -0,0 +1,9 @@
+extends RefCounted
+
+var alive: Array[bool] = []
+var pos_x: PackedInt32Array = PackedInt32Array()
+var pos_y: PackedInt32Array = PackedInt32Array()
+var resource_type: PackedStringArray = PackedStringArray()
+var amount: PackedInt32Array = PackedInt32Array()
+var ttl: PackedInt32Array = PackedInt32Array()
+
