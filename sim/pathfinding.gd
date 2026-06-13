c187aa41dfd591345ee44cf424715e34cd14c926
dmitriikomarskikh-coder <dmitrii.komarskikh@gmail.com>
2026-06-13 11:14:41 +0300

    Add sim/pathfinding.gd

---
+++ sim/pathfinding.gd
@@ -0,0 +1,13 @@
+extends RefCounted
+
+const NEIGHBORS := [
+	Vector2i(0, -1),
+	Vector2i(1, 0),
+	Vector2i(0, 1),
+	Vector2i(-1, 0),
+	Vector2i(1, -1),
+	Vector2i(1, 1),
+	Vector2i(-1, 1),
+	Vector2i(-1, -1),
+]
+
